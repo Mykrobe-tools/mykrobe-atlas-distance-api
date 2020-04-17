b511373319ccaad929043033ab9ab49c81155d23
Giang Nguyen <giang@ebi.ac.uk>
2020-04-17 13:33:20 +0100

    Replace MySQL with Neo4j

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -1,9 +1,8 @@
 import connexion
 
-from swagger_server.__main__ import get_db
 from swagger_server.models import ApiResponse
 from swagger_server.models.sample import Sample  # noqa: E501
-from swagger_server.orm.DistanceORM import DistanceORM
+from swagger_server.orm.DistanceORM import SampleNode
 
 
 def distance_post(body):  # noqa: E501
@@ -19,12 +18,13 @@ def distance_post(body):  # noqa: E501
     if connexion.request.is_json:
         sample = Sample.from_dict(connexion.request.get_json())  # noqa: E501
 
-        within_distance = get_db().query(DistanceORM).filter(
-            DistanceORM.s1 == sample.experiment_id and DistanceORM.d < 5
-        ).all()
+        neighbors = SampleNode.nodes.filter(name=sample.experiment_id)\
+            .neighbors.match(dist__lt=10)
 
         return ApiResponse(
             type='distance',
             sub_type='nearest-neighbor',
-            result={sample.s2: sample.d for sample in within_distance}
+            result=[{
+                'name': s.name
+            } for s in neighbors]
         )
