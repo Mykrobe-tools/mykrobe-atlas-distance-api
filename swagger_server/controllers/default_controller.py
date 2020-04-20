272d847dddbb1111e82b564468304cb46435db0f
Giang Nguyen <giang@ebi.ac.uk>
2020-04-20 14:28:18 +0100

    Fix API response

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -1,8 +1,8 @@
 import connexion
+from neomodel import db
 
 from swagger_server.models import ApiResponse
 from swagger_server.models.sample import Sample  # noqa: E501
-from swagger_server.orm.DistanceORM import SampleNode
 
 
 def distance_post(body):  # noqa: E501
@@ -18,13 +18,15 @@ def distance_post(body):  # noqa: E501
     if connexion.request.is_json:
         sample = Sample.from_dict(connexion.request.get_json())  # noqa: E501
 
-        neighbors = SampleNode.nodes.filter(name=sample.experiment_id)\
-            .neighbors.match(dist__lt=10)
+        results, _ = db.cypher_query(
+            'MATCH (a:SampleNode)-[n:NEIGHBOR]-(b:SampleNode) '
+            'WHERE a.name={sample_name} AND n.dist < 10 '
+            'RETURN b, n.dist',
+            {'sample_name': sample.experiment_id}
+        )
 
         return ApiResponse(
             type='distance',
             sub_type='nearest-neighbor',
-            result=[{
-                'name': s.name
-            } for s in neighbors]
+            result={r[0]['name']: r[1] for r in results}
         )
