735bd6675ac2412e88c23e5a8e4507abc4ec6f4e
Giang Nguyen <giang@ebi.ac.uk>
2020-05-06 13:47:20 +0100

    Rewrite initdb script to not depend on neomodel

---
+++ swagger_server/helpers/db.py
@@ -0,0 +1,26 @@
+from threading import local
+
+from neo4j import GraphDatabase
+
+
+URI = ""
+ENCRYPTED = True
+
+
+class Database(local):
+    db = None
+
+    def __init__(self):
+        self.driver = GraphDatabase.driver(URI, encrypted=ENCRYPTED)
+
+    def query(self, q):
+        with self.driver.session() as s:
+            result = s.write_transaction(lambda tx: tx.run(q))
+
+        return result
+
+    @classmethod
+    def get(cls):
+        if not cls.db:
+            cls.db = Database()
+        return cls.db
