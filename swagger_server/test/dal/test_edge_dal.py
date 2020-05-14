d316218c72a4ebeb1098adec266b112a364edf43
Giang Nguyen <giang@ebi.ac.uk>
2020-05-14 21:54:12 +0100

    Test add graph with depth

--- swagger_server/test/dal/test_edge_dal.py
+++ swagger_server/test/dal/test_edge_dal.py
@@ -65,6 +65,42 @@ class TestEdgeDAL(BaseDALTestCase):
         rows = db.Neo4jDatabase.get().query(f'MATCH (n) RETURN n').values()
         self.assertEqual(4, len(rows))
 
+    def test_creating_paths(self):
+        """
+                   node
+                /       \
+            node         node
+           /     \       /    \
+        node     node   node   node
+        """
+
+        self.check_empty_db()
+
+        a, b, c, d, e, f, g = (Neo4jNode(properties={'name': 'n%d' % i}) for i in range(7))
+        edge_label = 'EDGE'
+        a.connect(b, edge_label)
+        a.connect(c, edge_label)
+        b.connect(d, edge_label)
+        b.connect(e, edge_label)
+        c.connect(f, edge_label)
+        c.connect(g, edge_label)
+        a.create()
+
+        rows = db.Neo4jDatabase.get().query(
+            f'MATCH '
+            f'({{name: "{a.properties["name"]}"}})-[e0:{edge_label}]->({{name: "{b.properties["name"]}"}}),'
+            f'({{name: "{a.properties["name"]}"}})-[e1:{edge_label}]->({{name: "{c.properties["name"]}"}}),'
+            f'({{name: "{b.properties["name"]}"}})-[e2:{edge_label}]->({{name: "{d.properties["name"]}"}}),'
+            f'({{name: "{b.properties["name"]}"}})-[e3:{edge_label}]->({{name: "{e.properties["name"]}"}}),'
+            f'({{name: "{c.properties["name"]}"}})-[e4:{edge_label}]->({{name: "{f.properties["name"]}"}}),'
+            f'({{name: "{c.properties["name"]}"}})-[e5:{edge_label}]->({{name: "{g.properties["name"]}"}})'
+            f' RETURN e0,e1,e2,e3,e4,e5'
+        ).values()
+        self.assertEqual(6, len(rows[0]))
+
+        rows = db.Neo4jDatabase.get().query(f'MATCH (n) RETURN n').values()
+        self.assertEqual(7, len(rows))
+
 
 if __name__ == '__main__':
     unittest.main()
