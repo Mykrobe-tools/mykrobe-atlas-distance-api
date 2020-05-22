bbde8c1fe4c67df9e4063d4ea34ab225d0a714be
Giang Nguyen <giang@ebi.ac.uk>
2020-05-22 23:59:34 +0100

    Use mypy config file

--- scripts/type_check.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-mypy --follow-imports=skip --ignore-missing-imports \
-    swagger_server/drivers \
-    swagger_server/ogm \
-    swagger_server/repositories \
-    swagger_server/services \
-    swagger_server/migrations
