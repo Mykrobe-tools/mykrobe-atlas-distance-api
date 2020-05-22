5bd999c149250cc24448e39a2bc4052e336f9d50
Giang Nguyen <giang@ebi.ac.uk>
2020-05-22 23:31:01 +0100

    Add type check to travis

--- scripts/type_check.sh
+++ scripts/type_check.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-mypy --follow-imports=skip --ignore-missing-imports swagger_server/drivers swagger_server/ogm swagger_server/repositories
+mypy --follow-imports=skip --ignore-missing-imports \
+    swagger_server/drivers \
+    swagger_server/ogm \
+    swagger_server/repositories \
+    swagger_server/services \
+    swagger_server/migrations
