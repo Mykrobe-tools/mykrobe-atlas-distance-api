94e6e8a1a29cfed3ec3c023d04cb4355c7104030
Giang Nguyen <giang@ebi.ac.uk>
2020-06-02 17:47:13 +0100

    refactor

--- swagger_server/controllers/samples_post_controller.py
+++ swagger_server/controllers/samples_post_controller.py
@@ -1,7 +1,6 @@
 import connexion
 
-from swagger_server import services
-from swagger_server.adapters.repositories.sample_repository import SampleAlreadyExist
+from swagger_server.adapters.repositories.sample_repository import SampleAlreadyExist, SampleRepository
 from swagger_server.databases.neo4j import Neo4jDatabase
 from swagger_server.models import Error
 from swagger_server.models.sample import Sample  # noqa: E501
@@ -21,9 +20,10 @@ def samples_post(body):  # noqa: E501
         body = Sample.from_dict(connexion.request.get_json())  # noqa: E501
 
     db = Neo4jDatabase()
+    repo = SampleRepository(db)
 
     try:
-        services.add_new_sample(body, db)
+        repo.add(body)
     except SampleAlreadyExist:
         return Error(409, 'Already existed'), 409
     else:
--- swagger_server/services.py
+++
@@ -1,8 +0,0 @@
-from swagger_server.adapters.repositories.sample_repository import SampleRepository
-from swagger_server.databases.base import BaseDatabase
-from swagger_server.models import Sample
-
-
-def add_new_sample(sample: Sample, db: BaseDatabase):
-    repo = SampleRepository(db)
-    repo.add(sample)
