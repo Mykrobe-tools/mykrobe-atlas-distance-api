743e6e5662dd72f2aeb46de0247f27b325207ea8
Giang Nguyen <giang@ebi.ac.uk>
2020-06-03 12:54:50 +0100

    Dup neighbours

--- swagger_server/test/e2e/test_samples_post_controller.py
+++ swagger_server/test/e2e/test_samples_post_controller.py
@@ -11,9 +11,7 @@ def test_creating_new_sample(db, client, sample):
         response = client.open('/api/v1/samples', method='POST', json=sample)
 
         assert response.status_code == 201
-
-        matcher = db.graph.nodes.match(SampleNode.__primarylabel__, name=sample.experiment_id)
-        assert len(matcher) == 1
+        assert len(db.graph.nodes.match(SampleNode.__primarylabel__, name=sample.experiment_id)) == 1
     finally:
         db.truncate()
 
@@ -30,7 +28,7 @@ def test_creating_duplicated_sample_returns_error(sample_repo, db, client):
 
 
 @given(sample=from_type(Sample), neighbours=lists(from_type(Neighbour), unique_by=lambda n: n.experiment_id))
-def test_creating_sample_with_non_duplicated_new_neighbours(db, client, sample, neighbours):
+def test_creating_sample_with_non_duplicated_and_new_neighbours(db, client, sample, neighbours):
     assume(sample.experiment_id not in [n.experiment_id for n in neighbours])
 
     sample.nearest_neighbours = neighbours
@@ -40,37 +38,67 @@ def test_creating_sample_with_non_duplicated_new_neighbours(db, client, sample, 
 
         assert response.status_code == 201
 
-        matcher = db.graph.nodes.match(SampleNode.__primarylabel__, name=sample.experiment_id)
-        assert len(matcher) == 1
+        sample_nodes = db.graph.nodes.match(SampleNode.__primarylabel__, name=sample.experiment_id)
+        assert len(sample_nodes) == 1
 
-        node = SampleNode.wrap(matcher.first())
+        node = SampleNode.wrap(sample_nodes.first())
         if sample.nearest_neighbours:
             assert len(node.neighbours) == len(sample.nearest_neighbours)
 
             for neighbour in sample.nearest_neighbours:
-                neighbour_node = [n for n in node.neighbours if n.name == neighbour.experiment_id][0]
-                assert node.neighbours.get(neighbour_node, 'distance') == neighbour.distance
+                neighbour_nodes = [n for n in node.neighbours if n.name == neighbour.experiment_id]
+                assert node.neighbours.get(neighbour_nodes[0], 'distance') == neighbour.distance
     finally:
         db.truncate()
 
 
 def test_existing_neighbours_are_merged(sample_repo, db, client):
-    existing_id = 'some id'
-    sample = Sample(existing_id)
+    sample_id = 'sample id'
+    neighbour_id = 'neighbour id'
+    neighbour_distance = 1
+
+    sample = Sample(neighbour_id)
     sample_repo.add(sample)
 
-    neighbour = Neighbour(existing_id, distance=1)
-    to_create = Sample('other id', nearest_neighbours=[neighbour])
-    response = client.open('/api/v1/samples', method='POST', json=to_create)
+    neighbour = Neighbour(neighbour_id, distance=neighbour_distance)
+    sample = Sample(sample_id, nearest_neighbours=[neighbour])
+
+    response = client.open('/api/v1/samples', method='POST', json=sample)
 
     assert response.status_code == 201
 
-    existing = db.graph.nodes.match(SampleNode.__primarylabel__, name=existing_id)
-    assert len(existing) == 1
+    neighbours = db.graph.nodes.match(SampleNode.__primarylabel__, name=neighbour_id)
+    assert len(neighbours) == 1
 
-    created = db.graph.nodes.match(SampleNode.__primarylabel__, name=to_create.experiment_id)
-    assert len(created) == 1
+    samples = db.graph.nodes.match(SampleNode.__primarylabel__, name=sample_id)
+    assert len(samples) == 1
 
-    relationship = db.graph.relationships.match(
-        [created.first(), existing.first()], NEIGHBOUR_REL_TYPE, distance=neighbour.distance)
-    assert len(relationship) == 1
+    relationships = db.graph.relationships.match(
+        [samples.first(), neighbours.first()], NEIGHBOUR_REL_TYPE, distance=neighbour_distance)
+    assert len(relationships) == 1
+
+
+def test_duplicated_neighbours_are_merged(db, client):
+    sample_id = 'sample id'
+    neighbour_id = 'neighbour id'
+    first_distance = 1
+    second_distance = 2
+
+    first_neighbour = Neighbour(neighbour_id, distance=first_distance)
+    second_neighbour = Neighbour(neighbour_id, distance=second_distance)
+    sample = Sample(sample_id, nearest_neighbours=[first_neighbour, second_neighbour])
+
+    response = client.open('/api/v1/samples', method='POST', json=sample)
+
+    assert response.status_code == 201
+
+    neighbours = db.graph.nodes.match(SampleNode.__primarylabel__, name=neighbour_id)
+    assert len(neighbours) == 1
+
+    samples = db.graph.nodes.match(SampleNode.__primarylabel__, name=sample_id)
+    assert len(samples) == 1
+
+    relationships = db.graph.relationships.match(
+        [samples.first(), neighbours.first()], NEIGHBOUR_REL_TYPE)
+    assert len(relationships) == 1
+    assert relationships.first()['distance'] == second_distance
