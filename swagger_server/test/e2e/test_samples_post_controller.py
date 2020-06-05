6e59ee0cd7e830d56388070ed3628c6f7f3a07ab
Giang Nguyen <giang@ebi.ac.uk>
2020-06-05 23:18:54 +0100

    r

--- swagger_server/test/e2e/test_samples_post_controller.py
+++ swagger_server/test/e2e/test_samples_post_controller.py
@@ -7,29 +7,14 @@ from swagger_server.test.e2e.utils import SAMPLES_API_PATH
 
 @given(sample=from_type(Sample))
 def test_create_new_sample(client, sample):
-    path = str(SAMPLES_API_PATH)
-    resp = client.open(path, method='POST', json=sample)
-    assert resp.status_code == 201
-
-    path = str(SAMPLES_API_PATH / sample.experiment_id)
-    resp = client.open(path)
-    assert resp.status_code == 200
-    assert resp.json == sample.to_dict()
+    check_create_and_retrieve_sample(sample, client)
 
 
 @given(sample=from_type(Sample), nearest_leaf=from_type(NearestLeaf))
 def test_create_new_sample_with_nearest_leaf(db, client, sample, nearest_leaf):
     try:
         sample.nearest_leaf_node = nearest_leaf
-
-        path = str(SAMPLES_API_PATH)
-        resp = client.open(path, method='POST', json=sample)
-        assert resp.status_code == 201
-
-        path = str(SAMPLES_API_PATH / sample.experiment_id)
-        resp = client.open(path)
-        assert resp.status_code == 200
-        assert resp.json == sample.to_dict()
+        check_create_and_retrieve_sample(sample, client)
     finally:
         db.delete_all()
 
@@ -38,14 +23,17 @@ def test_create_new_sample_with_nearest_leaf(db, client, sample, nearest_leaf):
 def test_create_new_sample_with_one_neighbour(db, client, sample, neighbour):
     try:
         sample.nearest_neighbours = [neighbour]
-
-        path = str(SAMPLES_API_PATH)
-        resp = client.open(path, method='POST', json=sample)
-        assert resp.status_code == 201
-
-        path = str(SAMPLES_API_PATH / sample.experiment_id)
-        resp = client.open(path)
-        assert resp.status_code == 200
-        assert resp.json == sample.to_dict()
+        check_create_and_retrieve_sample(sample, client)
     finally:
         db.delete_all()
+
+
+def check_create_and_retrieve_sample(sample, client):
+    path = str(SAMPLES_API_PATH)
+    resp = client.open(path, method='POST', json=sample)
+    assert resp.status_code == 201
+
+    path = str(SAMPLES_API_PATH / sample.experiment_id)
+    resp = client.open(path)
+    assert resp.status_code == 200
+    assert resp.json == sample.to_dict()
