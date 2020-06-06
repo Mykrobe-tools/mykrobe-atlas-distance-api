4eeac53014f981abe153234f1d329a4cf2475090
Giang Nguyen <giang@ebi.ac.uk>
2020-06-06 02:27:15 +0100

    r

--- swagger_server/test/e2e/test_samples_post_controller.py
+++ swagger_server/test/e2e/test_samples_post_controller.py
@@ -1,5 +1,3 @@
-from copy import deepcopy
-
 from hypothesis import given, assume
 from hypothesis.strategies import from_type, lists, integers
 
@@ -9,23 +7,24 @@ from swagger_server.test.e2e.utils import SAMPLES_API_PATH
 
 @given(sample=from_type(Sample))
 def test_create_new_sample(client, sample):
-    check_create_and_retrieve_sample(sample, client)
+    assert_created_and_retrieved_samples_are_the_same(sample, client)
 
 
 @given(sample=from_type(Sample), nearest_leaf=from_type(NearestLeaf))
 def test_create_new_sample_with_nearest_leaf(db, client, sample, nearest_leaf):
     try:
         sample.nearest_leaf_node = nearest_leaf
-        check_create_and_retrieve_sample(sample, client)
+        assert_created_and_retrieved_samples_are_the_same(sample, client)
     finally:
         db.delete_all()
 
 
 @given(sample=from_type(Sample), neighbour=from_type(Neighbour))
 def test_create_new_sample_with_one_neighbour(db, client, sample, neighbour):
+    assume(sample.experiment_id != neighbour.experiment_id)
     try:
         sample.nearest_neighbours = [neighbour]
-        check_create_and_retrieve_sample(sample, client)
+        assert_created_and_retrieved_samples_are_the_same(sample, client)
     finally:
         db.delete_all()
 
@@ -35,7 +34,7 @@ def test_create_new_sample_with_multiple_unique_neighbours(db, client, sample, n
     assume(sample.experiment_id not in [x.experiment_id for x in neighbours])
     try:
         sample.nearest_neighbours = neighbours
-        check_create_and_retrieve_sample(sample, client)
+        assert_created_and_retrieved_samples_are_the_same(sample, client)
     finally:
         db.delete_all()
 
@@ -44,12 +43,13 @@ def test_create_new_sample_with_multiple_unique_neighbours(db, client, sample, n
 def test_create_new_sample_that_neighbour_itself(db, client, sample, distance):
     try:
         sample.nearest_neighbours = [Neighbour(sample.experiment_id, distance)]
-        check_create_and_retrieve_sample(sample, client)
+        actual = create_and_retrieve_sample(sample, client)
+        assert not actual.nearest_neighbours
     finally:
         db.delete_all()
 
 
-def check_create_and_retrieve_sample(sample, client):
+def create_and_retrieve_sample(sample, client):
     path = str(SAMPLES_API_PATH)
     resp = client.open(path, method='POST', json=sample)
     assert resp.status_code == 201
@@ -58,17 +58,16 @@ def check_create_and_retrieve_sample(sample, client):
     resp = client.open(path)
     assert resp.status_code == 200
 
-    actual = Sample.from_dict(resp.json)
-    expected = deepcopy(sample)
-    expected.nearest_neighbours = [x for x in expected.nearest_neighbours if x.experiment_id != expected.experiment_id]
+    return Sample.from_dict(resp.json)
 
-    assert actual.experiment_id == expected.experiment_id
-    assert actual.nearest_leaf_node == expected.nearest_leaf_node
-    assert bool(actual.nearest_neighbours) == bool(expected.nearest_neighbours)
 
-    if expected.nearest_neighbours:
-        for x in expected.nearest_neighbours:
-            if x.experiment_id != sample.experiment_id:
-                assert x in actual.nearest_neighbours
-            else:
-                assert x not in actual.nearest_neighbours
+def assert_created_and_retrieved_samples_are_the_same(sample, client):
+    actual = create_and_retrieve_sample(sample, client)
+
+    assert actual.experiment_id == sample.experiment_id
+    assert actual.nearest_leaf_node == sample.nearest_leaf_node
+    assert bool(actual.nearest_neighbours) == bool(sample.nearest_neighbours)
+
+    if sample.nearest_neighbours and actual.nearest_neighbours:
+        for x in sample.nearest_neighbours:
+            assert x in actual.nearest_neighbours
