094b83b1a05c78623f20567353fda3a35a714740
Giang Nguyen <giang@ebi.ac.uk>
2020-06-11 19:21:18 +0100

    r

--- swagger_server/factories.py
+++ swagger_server/factories.py
@@ -2,9 +2,37 @@ from functools import singledispatchmethod
 
 from py2neo.ogm import GraphObject
 
-from swagger_server.models import Sample
+from swagger_server.models import Sample, NearestLeaf, Neighbour
 from swagger_server.models.base_model_ import Model
 from swagger_server.ogm import LeafNode, SampleNode
+
+
+class ModelFactory:
+    @singledispatchmethod
+    @staticmethod
+    def build(recipe: GraphObject) -> Model:
+        raise NotImplementedError
+
+    @build.register(SampleNode)
+    @staticmethod
+    def _(recipe: SampleNode) -> Sample:
+        leaf_relationship = recipe.lineage
+        neighbour_relationships = recipe.neighbours
+
+        sample = Sample(recipe.experiment_id)
+
+        if len(leaf_relationship) > 0:
+            leaf_node = next(iter(leaf_relationship))
+            distance = recipe.lineage.get(leaf_node, 'distance')
+            sample.nearest_leaf_node = NearestLeaf(leaf_node.leaf_id, distance)
+
+        if len(neighbour_relationships) > 0:
+            sample.nearest_neighbours = []
+            for neighbour_node in neighbour_relationships:
+                distance = recipe.neighbours.get(neighbour_node, 'distance')
+                sample.nearest_neighbours.append(Neighbour(neighbour_node.experiment_id, distance))
+
+        return sample
 
 
 class GraphFactory:
--- swagger_server/services/graph.py
+++ swagger_server/services/graph.py
@@ -1,7 +1,7 @@
 from py2neo import Graph
 
-from swagger_server.factories import GraphFactory
-from swagger_server.models import Sample, NearestLeaf, Neighbour
+from swagger_server.factories import GraphFactory, ModelFactory
+from swagger_server.models import Sample
 from swagger_server.ogm import SampleNode
 
 
@@ -10,26 +10,9 @@ def create_sample(sample: Sample, db: Graph):
     node.create(db)
 
 
-def get_sample(experiment_id: str, db: Graph) -> Sample:
+def get_sample(experiment_id: str, db: Graph):
     sample_node = SampleNode.get(experiment_id, db)
-
-    leaf_relationship = sample_node.lineage
-    neighbour_relationships = sample_node.neighbours
-
-    sample = Sample(sample_node.experiment_id)
-
-    if len(leaf_relationship) > 0:
-        leaf_node = next(iter(leaf_relationship))
-        distance = sample_node.lineage.get(leaf_node, 'distance')
-        sample.nearest_leaf_node = NearestLeaf(leaf_node.leaf_id, distance)
-
-    if len(neighbour_relationships) > 0:
-        sample.nearest_neighbours = []
-        for neighbour_node in neighbour_relationships:
-            distance = sample_node.neighbours.get(neighbour_node, 'distance')
-            sample.nearest_neighbours.append(Neighbour(neighbour_node.experiment_id, distance))
-
-    return sample
+    return ModelFactory.build(sample_node)
 
 
 def delete_sample(experiment_id: str, db: Graph):
