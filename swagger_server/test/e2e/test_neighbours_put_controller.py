623df1062beff46253ae1e730e730b5b26b6b05e
Giang Nguyen <giang@ebi.ac.uk>
2020-06-17 14:24:36 +0100

    make test clearer

--- swagger_server/test/e2e/test_neighbours_put_controller.py
+++ swagger_server/test/e2e/test_neighbours_put_controller.py
@@ -23,20 +23,36 @@ def test_updating_neighbours(neo4j, client, sample, neighbours, nearest_leaf):
 
     try:
         sample.nearest_leaf_node = nearest_leaf
-        path = str(SAMPLES_API_PATH)
-        client.open(path, method='POST', json=sample)
+        create_sample(sample, client)
 
-        path = str(NEIGHBOURS_API_PATH(sample.experiment_id))
-        resp = client.open(path, method='PUT', json=neighbours)
+        resp = update_neighbours(sample, neighbours, client)
         assert resp.status_code == 200
 
-        actual = [Neighbour.from_dict(n) for n in resp.json]
-        assert actual == neighbours
+        updated_neighbours = [Neighbour.from_dict(n) for n in resp.json]
+        assert updated_neighbours == neighbours
 
-        path = str(NEIGHBOURS_API_PATH(sample.experiment_id))
-        resp = client.open(path)
-        actual = [Neighbour.from_dict(x) for x in resp.json]
-        for x in neighbours:
-            assert x in actual
+        resp = get_neighbours(sample, client)
+        if not neighbours:
+            assert resp.status_code == 404
+        else:
+            actual_neighbours = [Neighbour.from_dict(x) for x in resp.json]
+            assert len(actual_neighbours) == len(neighbours)
+            for x in neighbours:
+                assert x in actual_neighbours
     finally:
         neo4j.truncate()
+
+
+def create_sample(sample, client):
+    path = str(SAMPLES_API_PATH)
+    client.open(path, method='POST', json=sample)
+
+
+def update_neighbours(sample, neighbours, client):
+    path = str(NEIGHBOURS_API_PATH(sample.experiment_id))
+    return client.open(path, method='PUT', json=neighbours)
+
+
+def get_neighbours(sample, client):
+    path = str(NEIGHBOURS_API_PATH(sample.experiment_id))
+    return client.open(path)
