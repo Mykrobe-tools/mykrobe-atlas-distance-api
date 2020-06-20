10c820c98d764eb96e017b95698c4273f8719b7e
Giang Nguyen <giang@ebi.ac.uk>
2020-06-20 03:40:52 +0100

    r

--- swagger_server/controllers/samples_post_controller.py
+++ swagger_server/controllers/samples_post_controller.py
@@ -1,11 +1,10 @@
 import connexion
 from flask import g
 
-from swagger_server.exceptions import Existed
 from swagger_server.factories import SampleFactory
 from swagger_server.models.error import Error  # noqa: E501
 from swagger_server.models.sample import Sample  # noqa: E501
-from swagger_server.ogm import SampleNode
+from swagger_server.ogm import SampleNode, LeafNode
 
 
 def samples_post(sample=None):  # noqa: E501
@@ -23,9 +22,25 @@ def samples_post(sample=None):  # noqa: E501
 
     sample_graph = g.sample_graph
 
-    try:
-        node = SampleNode.create_from(sample, sample_graph)
-    except Existed:
+    node = SampleNode()
+    node.experiment_id = sample.experiment_id
+
+    if sample.nearest_leaf_node:
+        n = LeafNode()
+        n.leaf_id = sample.nearest_leaf_node.leaf_id
+        if n.exists(sample_graph):
+            node.lineage.add(n, distance=sample.nearest_leaf_node.distance)
+
+    if sample.nearest_neighbours:
+        for neighbour in sample.nearest_neighbours:
+            if neighbour.experiment_id != sample.experiment_id:
+                n = SampleNode()
+                n.experiment_id = neighbour.experiment_id
+                if n.exists(sample_graph):
+                    node.neighbours.add(n, distance=neighbour.distance)
+
+    if node.exists(sample_graph):
         return Error(409, 'Already existed'), 409
-    else:
-        return SampleFactory.build(node), 201
+    sample_graph.push(node)
+
+    return SampleFactory.build(node), 201
--- swagger_server/ogm.py
+++ swagger_server/ogm.py
@@ -1,16 +1,13 @@
 from py2neo import Graph
 from py2neo.ogm import GraphObject, Property, RelatedTo
 
-from swagger_server.exceptions import Existed
-from swagger_server.models import Sample
-
 
 class BaseGraphObject(GraphObject):
     def exists(self, graph: Graph) -> bool:
         return len(self.match(graph, self.__primaryvalue__)) > 0
 
 
-class LeafNode(GraphObject):
+class LeafNode(BaseGraphObject):
     __primarykey__ = 'leaf_id'
 
     leaf_id = Property()
@@ -23,29 +20,3 @@ class SampleNode(BaseGraphObject):
 
     neighbours = RelatedTo('SampleNode', 'NEIGHBOUR')
     lineage = RelatedTo(LeafNode, 'LINEAGE')
-
-    @classmethod
-    def create_from(cls, sample: Sample, graph: Graph):
-        node = cls()
-        node.experiment_id = sample.experiment_id
-
-        if node.exists(graph):
-            raise Existed
-
-        graph.push(node)
-
-        if sample.nearest_leaf_node and len(LeafNode.match(graph, sample.nearest_leaf_node.leaf_id)) > 0:
-            n = LeafNode()
-            n.leaf_id = sample.nearest_leaf_node.leaf_id
-            node.lineage.add(n, distance=sample.nearest_leaf_node.distance)
-
-        if sample.nearest_neighbours:
-            for neighbour in sample.nearest_neighbours:
-                if neighbour.experiment_id != sample.experiment_id and len(SampleNode.match(graph, neighbour.experiment_id)) > 0:
-                    n = SampleNode()
-                    n.experiment_id = neighbour.experiment_id
-                    node.neighbours.add(n, distance=neighbour.distance)
-
-        graph.push(node)
-
-        return node
