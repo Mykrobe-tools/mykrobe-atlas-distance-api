196a62337fb29a1498e253db0a260e816fdb4c2e
Giang Nguyen <giang@ebi.ac.uk>
2020-06-22 13:06:40 +0100

    r

--- swagger_server/test/e2e/test_samples_post_controller.py
+++ swagger_server/test/e2e/test_samples_post_controller.py
@@ -1,120 +1,96 @@
 from hypothesis import given, assume
-from pytest import fixture
 
 from swagger_server.models import Sample
-from swagger_server.test.strategies import samples, neighbours
+from swagger_server.test.strategies import samples
 
 
-class TestSampleExists:
-    @fixture(scope='class')
-    def existing_sample(self, create_sample, sample_graph):
-        sample = Sample('0')
-        create_sample(sample, ensure=True)
-        yield sample
-        sample_graph.delete_all()
+@given(old=samples(), new=samples())
+def test_creating_sample_with_existing_experiment_id(old, new, create_sample, sample_graph):
+    try:
+        create_sample(old, ensure=True)
 
-    @given(sample=samples())
-    def test_endpoint_returns_409(self, sample, existing_sample, create_sample):
-        sample.experiment_id = existing_sample.experiment_id
+        new.experiment_id = old.experiment_id
 
-        response = create_sample(existing_sample)
+        response = create_sample(new)
 
-        assert response.status_code == 409, response.data.decode()
-
-    @given(sample=samples())
-    def test_nothing_is_created(self, sample, existing_sample, create_sample, sample_graph):
-        sample.experiment_id = existing_sample.experiment_id
-
-        create_sample(sample)
+        assert response.status_code == 409
 
         assert len(sample_graph.nodes) == 1
         assert len(sample_graph.relationships) == 0
+    finally:
+        sample_graph.delete_all()
 
 
-class TestLeafAndNeighboursDoNotExist:
-    @given(sample=samples())
-    def test_sample_is_created(self, sample, create_sample, sample_graph):
-        try:
-            response = create_sample(sample)
-            created = Sample.from_dict(response.json)
+@given(sample=samples())
+def test_creating_sample_with_relationships_to_non_existent_nodes(sample, create_sample, sample_graph):
+    try:
+        response = create_sample(sample)
+        created = Sample.from_dict(response.json)
 
-            assert response.status_code == 201, response.data.decode()
-            assert created.experiment_id == sample.experiment_id
+        assert response.status_code == 201
 
-            assert len(sample_graph.nodes) == 1
-        finally:
-            sample_graph.delete_all()
+        assert created.experiment_id == sample.experiment_id
+        assert not created.nearest_neighbours
+        assert not created.nearest_leaf_node
 
-    @given(sample=samples())
-    def test_relationships_are_not_created(self, sample, create_sample, sample_graph):
-        try:
-            response = create_sample(sample, ensure=True)
-            created = Sample.from_dict(response.json)
-
-            assert not created.nearest_neighbours
-            assert not created.nearest_leaf_node
-
-            assert len(sample_graph.relationships) == 0
-        finally:
-            sample_graph.delete_all()
+        assert len(sample_graph.nodes) == 1
+        assert len(sample_graph.relationships) == 0
+    finally:
+        sample_graph.delete_all()
 
 
-class TestLeafAndNeighboursExist:
-    @given(sample=samples())
-    def test_duplicated_neighbour_relationships_are_not_created(self, sample, create_sample, sample_graph):
-        assume(sample.nearest_neighbours)
-        assume(sample.experiment_id not in [x.experiment_id for x in sample.nearest_neighbours])
+@given(sample=samples(has_neighbours=True))
+def test_duplicated_neighbour_relationships_are_not_created(sample, create_sample, sample_graph):
+    assume(sample.experiment_id not in [x.experiment_id for x in sample.nearest_neighbours])
 
-        unique_neighbour_ids = set([x.experiment_id for x in sample.nearest_neighbours])
+    unique_neighbour_ids = set([x.experiment_id for x in sample.nearest_neighbours])
 
-        try:
+    try:
+        for neighbour in sample.nearest_neighbours:
+            create_sample(neighbour)
+
+        response = create_sample(sample, ensure=True)
+        created = Sample.from_dict(response.json)
+
+        assert len(created.nearest_neighbours) == len(unique_neighbour_ids)
+        assert len(sample_graph.relationships) == len(unique_neighbour_ids)
+    finally:
+        sample_graph.delete_all()
+
+
+@given(sample=samples(has_neighbours=True, unique_neighbours=True))
+def test_self_neighbouring_relationships_are_not_created(sample, create_sample, sample_graph):
+    valid_neighbours = [x for x in sample.nearest_neighbours if x.experiment_id != sample.experiment_id]
+
+    try:
+        for neighbour in valid_neighbours:
+            create_sample(neighbour, ensure=True)
+
+        response = create_sample(sample, ensure=True)
+        created = Sample.from_dict(response.json)
+
+        assert len(created.nearest_neighbours) == len(valid_neighbours)
+        assert len(sample_graph.relationships) == len(valid_neighbours)
+    finally:
+        sample_graph.delete_all()
+
+
+@given(sample=samples())
+def test_created_sample_matches_retrieved_sample(sample, create_sample, create_leaf, get_sample, sample_graph):
+    try:
+        if sample.nearest_neighbours:
             for neighbour in sample.nearest_neighbours:
-                create_sample(neighbour)
-
-            response = create_sample(sample, ensure=True)
-            created = Sample.from_dict(response.json)
-
-            assert len(created.nearest_neighbours) == len(unique_neighbour_ids)
-            assert len(sample_graph.relationships) == len(unique_neighbour_ids)
-        finally:
-            sample_graph.delete_all()
-
-    @given(sample=samples(unique_neighbours=True), self_neighbouring_relationship=neighbours())
-    def test_self_neighbouring_relationships_are_not_created(self, sample, self_neighbouring_relationship, create_sample, sample_graph):
-        assume(sample.nearest_neighbours)
-        assume(sample.experiment_id not in [x.experiment_id for x in sample.nearest_neighbours])
-
-        try:
-            for neighbour in sample.nearest_neighbours:
-                create_sample(neighbour, ensure=True)
-
-            self_neighbouring_relationship.experiment_id = sample.experiment_id
-            sample.nearest_neighbours.append(self_neighbouring_relationship)
-
-            response = create_sample(sample, ensure=True)
-            created = Sample.from_dict(response.json)
-
-            assert created.experiment_id == sample.experiment_id
-            assert self_neighbouring_relationship not in created.nearest_neighbours
-        finally:
-            sample_graph.delete_all()
-
-    @given(sample=samples(has_neighbours=True, unique_neighbours=True, has_leaf=True))
-    def test_all_valid_relationships_are_created(self, sample, create_sample, create_leaf, sample_graph):
-        assume(sample.experiment_id not in [x.experiment_id for x in sample.nearest_neighbours])
-
-        try:
-            for neighbour in sample.nearest_neighbours:
-                create_sample(neighbour, ensure=True)
+                if neighbour.experiment_id != sample.experiment_id:
+                    create_sample(neighbour)
+        if sample.nearest_leaf_node:
             create_leaf(sample.nearest_leaf_node, ensure=True)
 
-            response = create_sample(sample, ensure=True)
-            created = Sample.from_dict(response.json)
+        created = Sample.from_dict(create_sample(sample, ensure=True).json)
+        retrieved = Sample.from_dict(get_sample(sample, ensure=True).json)
 
-            assert created.nearest_neighbours == sample.nearest_neighbours
-            assert created.nearest_leaf_node == sample.nearest_leaf_node
-
-            assert len(sample_graph.nodes) == len(created.nearest_neighbours) + 2
-            assert len(sample_graph.relationships) == len(created.nearest_neighbours) + 1
-        finally:
-            sample_graph.delete_all()
+        assert created.nearest_leaf_node == retrieved.nearest_leaf_node
+        assert len(created.nearest_neighbours) == len(retrieved.nearest_neighbours)
+        for neighbour in created.nearest_neighbours:
+            assert neighbour in retrieved.nearest_neighbours
+    finally:
+        sample_graph.delete_all()
