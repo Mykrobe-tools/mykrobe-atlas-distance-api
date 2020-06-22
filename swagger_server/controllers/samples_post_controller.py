674d39170175b5388b2138106291bbd64df82c15
Giang Nguyen <giang@ebi.ac.uk>
2020-06-22 21:55:34 +0100

    r

--- swagger_server/controllers/samples_post_controller.py
+++ swagger_server/controllers/samples_post_controller.py
@@ -2,10 +2,10 @@ import connexion
 from flask import g
 
 from swagger_server.exceptions import Existed
-from swagger_server.factories import SampleFactory
 from swagger_server.models.error import Error  # noqa: E501
 from swagger_server.models.sample import Sample  # noqa: E501
 from swagger_server.services import create_sample
+from swagger_server.validators import SampleValidator
 
 
 def samples_post(sample=None):  # noqa: E501
@@ -20,6 +20,10 @@ def samples_post(sample=None):  # noqa: E501
     """
     if connexion.request.is_json:
         sample = Sample.from_dict(connexion.request.get_json())  # noqa: E501
+
+    valid, error = SampleValidator(sample).valid()
+    if not valid:
+        return Error(400, error), 400
 
     sample_graph = g.sample_graph
 
