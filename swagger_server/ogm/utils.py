24c820b27c129bc5baa2801c5a1f7d2765f514bb
Giang Nguyen <giang@ebi.ac.uk>
2021-08-31 15:49:57 +0100

    Cleaner retry logic

--- swagger_server/ogm/utils.py
+++ swagger_server/ogm/utils.py
@@ -1,27 +1,13 @@
 def with_retry(exception_class, max_retries=3):
     def outer_wrapper(method):
         def wrapper(instance, *args, **kwargs):
-            try:
-                return method(instance, *args, **kwargs)
-            except exception_class:
-                if max_retries == 0:
-                    raise
-
-                done = False
-
-                for count in range(max_retries):
-                    if done:
-                        break
-
-                    while True:
-                        try:
-                            return method(instance, *args, **kwargs)
-                        except exception_class:
-                            if count == max_retries - 1:
-                                raise
-
-                            count += 1
-                            continue
+            count = 0
+            while count < max_retries:
+                try:
+                    return method(instance, *args, **kwargs)
+                except exception_class:
+                    count += 1
+            return method(instance, *args, **kwargs)
 
         return wrapper
     return outer_wrapper
